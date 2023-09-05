5a990237239841abbddffcafa41a06313e1ca9d6
chanikya1111 <chanikyareddy1508@gmail.com>
2023-09-05 18:15:39 +0530

    typecasting entered

---
+++ PRACTISETYPECASTING.py
@@ -0,0 +1,24 @@
+#TYPECASTING
+'''
+#INT-->FLOAT
+
+X=10
+y=float(X)
+print(X,"type is:",y,type(y))
+
+#Float -->Int
+
+x=20.6
+y=int(x)
+print(x,"type is:",type(y),y)
+'''
+#int-->string
+x=76
+y=str(x)
+print(x,"type is",type(y),y)
+#string-->int
+x='20.6'
+y=float(x)
+print(x,"type is:",type(y),y)
+z=int(y)
+print(y,"type is:",type(z),z)
