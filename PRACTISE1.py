eef36e53199adb9a3be486555f0527b6b1531aa7
chanikya1111 <chanikyareddy1508@gmail.com>
2023-09-05 17:47:18 +0530

    datatypes entered

---
+++ PRACTISE1.py
@@ -0,0 +1,19 @@
+#Datatypes
+
+'''
+#int
+x=10
+print(type(x))
+#float
+y=10.2
+print(type(y))
+
+#string
+z='chanikya'
+print(type(z))
+#boolean
+zz=True
+print(type(zz))
+'''
+#type casting
+#int-->float
